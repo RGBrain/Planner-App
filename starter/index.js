389013e9a12b46d29b116daccb68dd6c9953f5e9
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-02 20:31:15 +0000

    add entry to local storage

--- starter/index.js
+++ starter/index.js
@@ -1,7 +1,7 @@
 // Display current date 
 var currentDate = moment().format("dddd Do MMMM");
-var currentHour = moment().format("HH")
-var currentDateEl = $('#currentDay')
+var currentHour = moment().format("HH");
+var currentDateEl = $('#currentDay');
 currentDateEl.text(currentDate);
 
 // Change colour based on time
@@ -17,3 +17,33 @@ textArea.each(function() {
         $(this).addClass('future');
     }
 })
+
+//
+// ? Describe the functionality:   
+// When the page loads, text area content should be loaded from local storage.
+// When I click on a button, any text IN THAT TEXT AREA should be saved to local storage.
+//
+
+var BtnEl = $('.saveBtn');
+
+BtnEl.click(function() {
+    // grab textarea next to clicked button
+    let textField = $(this).prev();
+    // get value of textarea
+    let notes = textField.val();
+    // ? debugging log
+    console.log("the text is: " + notes);
+    //store in local storage with data-time as key
+    localStorage.setItem($(this).prev().attr('data-time'), notes);
+    // ? debugging log
+    console.log($(this).prev().attr('data-time'))
+});
+
+
+// ! DEBUG 
+let lastname09 = localStorage.getItem('09');
+let lastname10 = localStorage.getItem('10');
+let lastname11 = localStorage.getItem('11');
+console.log("local storage09: " + lastname09);
+console.log("local storage10: " + lastname10);
+console.log("local storage11: " + lastname11);
