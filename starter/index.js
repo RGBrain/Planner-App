54bd7a749c6da0fafff6ae8f7d8c03d555708898
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-02 20:49:22 +0000

    get entries from local storage

--- starter/index.js
+++ starter/index.js
@@ -18,12 +18,13 @@ textArea.each(function() {
     }
 })
 
-//
-// ? Describe the functionality:   
-// When the page loads, text area content should be loaded from local storage.
-// When I click on a button, any text IN THAT TEXT AREA should be saved to local storage.
-//
+// get notes from local storage
+textArea.each(function() {
+    $(this).val(localStorage.getItem($(this).attr('data-time')));
+})
 
+
+// Save to local storage
 var BtnEl = $('.saveBtn');
 
 BtnEl.click(function() {
@@ -31,19 +32,15 @@ BtnEl.click(function() {
     let textField = $(this).prev();
     // get value of textarea
     let notes = textField.val();
-    // ? debugging log
-    console.log("the text is: " + notes);
     //store in local storage with data-time as key
     localStorage.setItem($(this).prev().attr('data-time'), notes);
-    // ? debugging log
-    console.log($(this).prev().attr('data-time'))
 });
 
 
 // ! DEBUG 
-let lastname09 = localStorage.getItem('09');
-let lastname10 = localStorage.getItem('10');
-let lastname11 = localStorage.getItem('11');
-console.log("local storage09: " + lastname09);
-console.log("local storage10: " + lastname10);
-console.log("local storage11: " + lastname11);
+// let lastname09 = localStorage.getItem('09');
+// let lastname10 = localStorage.getItem('10');
+// let lastname11 = localStorage.getItem('11');
+// console.log("local storage09: " + lastname09);
+// console.log("local storage10: " + lastname10);
+// console.log("local storage11: " + lastname11);
